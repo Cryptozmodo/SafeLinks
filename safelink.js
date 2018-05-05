31fb472d2269682de6e3e0ef5fa733ed4d1ea951
Cryptozmodo <38927696+Cryptozmodo@users.noreply.github.com>
2018-05-05 22:52:35 +0800

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,8 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="go.html"}
 if(x=="2"){xxx="go.html"}
 if(x=="3"){xxx="go.html"}
-if(x=="4"){xxx="go.html"}
-a_to_ve[i].href="https://coinsafelinks2.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="go.html"}a_to_ve[i].href="https://coinsafelinks2.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
