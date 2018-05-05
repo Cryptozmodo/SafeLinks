9350f641df9bf69d47845cb19d2a8cca5d5b1d9f
Cryptozmodo <38927696+Cryptozmodo@users.noreply.github.com>
2018-05-06 03:36:45 +0800

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,11 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="link01.html"}
-if(x=="2"){xxx="link02.html"}
-if(x=="3"){xxx="link03.html"}
-if(x=="4"){xxx="link04.html"}
-a_to_ve[i].href="https://ampmaster.github.io/safelink/go/to/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="go.html"}
+if(x=="2"){xxx="go.html"}
+if(x=="3"){xxx="go.html"}
+if(x=="4"){xxx="go.html"}a_to_ve[i].href="https://coinsafelinks2.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
